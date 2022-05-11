483d261a139908fa69103e7cc48c41da451dcac4
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-11 14:17:23 +0530

    adding UC8

---
+++ EmployeeWageUC8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputeEmployeeWage computeEmployeeWage = new ComputeEmployeeWage();
+            ComputeEmployeeWage.ComputeEmpWage("Dmat", 20, 2, 10);
+            ComputeEmployeeWage.ComputeEmpWage("Reliance", 10, 4, 20);
+        }
+
+    }
+}
